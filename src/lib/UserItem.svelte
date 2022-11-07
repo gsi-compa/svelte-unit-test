6705649c6cb003769651cc7d5b56496cdd12df51
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2022-11-06 21:51:39 -0500

    First steps

---
+++ src/lib/UserItem.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let user = null;
+</script>
+
+<li class="user-item">
+  <div
+    class={user.isActive
+      ? "user-item__active-indicator is-active"
+      : "user-item__active-indicator"}
+  />
+  <p class="user-item__text">
+    {user.firstName}
+    {user.lastName}
+  </p>
+</li>
+
+<style lang="scss">
+  .user-item {
+    display: flex;
+    align-items: center;
+    &__active-indicator {
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      background-color: red;
+      margin-right: 5px;
+      &.is-active {
+        background-color: green;
+      }
+    }
+  }
+</style>
---
+++ src/lib/UsersList.svelte
@@ -0,0 +1,16 @@
+<script>
+  import UserItem from "./UserItem.svelte";
+  export let initialUsers = [
+    {
+      firstName: "José Gabriel",
+      lastName: "Companioni",
+      isActive: true,
+    },
+  ];
+</script>
+
+<ul>
+  {#each initialUsers as user, index}
+    <UserItem {user} />
+  {/each}
+</ul>
